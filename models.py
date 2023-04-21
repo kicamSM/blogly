49a7ee425f2e8bbd01af22865d1309ab7f875538
kicamSM <kicamSMM@hotmail.com>
2023-04-21 15:46:20 -0600

    Blogly Part III seed.py error

--- models.py
+++ models.py
@@ -10,7 +10,8 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
-    
+
+# Users are like departments each user can have many posts but each post only has one user   
 class User(db.Model):
     __tablename__ = 'users'
     
@@ -25,6 +26,7 @@ class User(db.Model):
         u = self
         return f"< user_id={u.user_id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}"
 
+# Post is like Employee each post has one user just like each Employee has one department
 class Post(db.Model):
     __tablename__ = 'posts'
     
@@ -35,7 +37,35 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 
     user = db.relationship('User', backref=backref("posts", cascade="all, delete-orphan"))
+    #  user and posts are connected 
+    
+    post_with_tag = db.relationship('PostTag', backref='post' )
+    # Post is connected to PostTag Not cause of error
+    
+    tags = db.relationship('Tag', secondary="post_tag", backref="posts")
+    # posts and tags are connected NOT CAUSE OF ERROR
+    
+    
     
     def __repr__(self):
         p = self
         return f"<Post id={p.id} title={p.title} content={p.content} user_id={p.user_id} created_at={p.created_at}>"
+
+# Tags is like projects we are connecting Tags and Posts just like projects and employees are connected
+class Tag(db.Model):
+    """Tag Model"""
+    __tablename__ = "tags"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # autoincrement=True this you took this out of tags - not sure if this is correct
+    name = db.Column(db.Text, nullable=False, unique=True)
+    
+    post_with_tag = db.relationship('PostTag', backref='tag' )
+    # Tag is connected to PostTag
+    
+class PostTag(db.Model):
+    """Post Tag Model"""
+    __tablename__ = "post_tag"
+    
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
--- seed.py
+++ seed.py
@@ -1,11 +1,17 @@
 """Seed file to make sample data for db"""
 
-from models import User, Post, db 
+from models import User, Post, Tag, PostTag, db 
 from app import app
 
 # create all tables 
-db.drop_all()
-db.create_all()
+# db.drop_all()
+# db.create_all()
+
+PostTag.query.delete()
+User.query.delete()
+Post.query.delete()
+Tag.query.delete()
+
 
 abe = User(user_id='1',first_name="Abe", last_name="Lincoln", image_url="http://placekitten.com/g/200/300")
 george = User(user_id='2',first_name="George", last_name="Washington", image_url="http://placekitten.com/g/300/300")
@@ -18,24 +24,27 @@ p3 = Post(title="Untimely Tweets", content="This is an important notification se
 p4 = Post(title="The Adams Family", content="Presidents Run in our Family", user_id='3')
 p5 = Post(title="The first is the best", content="Declaration of Independence", user_id='2')
 
-# p1 = Post(title="On Slavery", content="Four score and seven years ago....", created_at="10/10/1830 @1345")
-# p2 = Post(title="On the Civil War", content="our forefathers....", created_at="10/10/1830 @1345")
-# p3 = Post(title="Untimely Tweets", content="This is an important notification sent out via Tweet", created_at="10/10/1830 @1345")
-# p4 = Post(title="The Adams Family", content="Presidents Run in our Family", created_at="10/10/1830 @1345")
-# p5 = Post(title="The first is the best", content="Declaration of Independence", created_at="10/10/1830 @1345")
+db.session.add_all([abe, george, samuel, donald, p1, p2, p3, p4, p5])
 
-db.session.add(abe)
-db.session.add(george)
-db.session.add(samuel)
-db.session.add(donald)
+# db.session.add(abe)
+# db.session.add(george)
+# db.session.add(samuel)
+# db.session.add(donald)
 
 db.session.commit()
 
-db.session.add(p1)
-db.session.add(p2)
-db.session.add(p3)
-db.session.add(p4)
-db.session.add(p5)
+# db.session.add(p1)
+# db.session.add(p2)
+# db.session.add(p3)
+# db.session.add(p4)
+# db.session.add(p5)
 
+# db.session.commit()
+
+ta = Tag(name='history', post_with_tag=[PostTag(post_id=p1.id)])
+
+tb = Tag(name='firsts', post_with_tag=[PostTag(post_id=p3.id)])
+# do you need a tag id in there???? will it be automated???
+
+db.session.add([ta, tb])
 db.session.commit()
-
